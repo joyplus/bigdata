911b995544536eb4843d3b6ad202fd696e45b028
yannanfei <415049053@qq.com>
2014-05-13 19:57:48 +0800

    接口

--- addata/src/main/java/tv/joyplus/backend/report/task/ReportTask.java
+++ addata/src/main/java/tv/joyplus/backend/report/task/ReportTask.java
@@ -1,5 +1,10 @@
 package tv.joyplus.backend.report.task;
 
+import tv.joyplus.backend.report.dao.JobResult;
+
 public interface ReportTask {
 
+	public String getJob();
+	public JobResult queryData(String strJobText);
+	public boolean saveData(JobResult result);
 }
