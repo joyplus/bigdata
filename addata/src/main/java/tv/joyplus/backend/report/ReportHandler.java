94c080617bc1c2a3da1cc0df25374e0dbb733f8f
yannanfei <415049053@qq.com>
2014-05-15 16:07:45 +0800

    beanstalk text获取

--- addata/src/main/java/tv/joyplus/backend/report/ReportHandler.java
+++ addata/src/main/java/tv/joyplus/backend/report/ReportHandler.java
@@ -1,12 +1,22 @@
 package tv.joyplus.backend.report;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.trendrr.beanstalk.BeanstalkClient;
+import com.trendrr.beanstalk.BeanstalkException;
+import com.trendrr.beanstalk.BeanstalkJob;
+
+import de.ailis.pherialize.MixedArray;
+import de.ailis.pherialize.Pherialize;
 import tv.joyplus.backend.report.task.ReportTask;
 
 
 public class ReportHandler {
+	
+	protected static Log log = LogFactory.getLog(ReportHandler.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,15 +24,22 @@ public class ReportHandler {
 		
 		ReportTask reportTask = (ReportTask) ctx.getBean("reportTask");
 		
-		reportTask.processReport(null);
+		BeanstalkClient bsClient = new BeanstalkClient("localhost", 11300, "tubeCustomizeReport");
 		
-		while (true) {
-			
-			
-			
+		try {
+			while (true) {
+				BeanstalkJob job = bsClient.reserve(1);
+				String strJobText = new String(job.getData());
+				MixedArray jobArr = Pherialize.unserialize(strJobText).toArray();
+				strJobText = jobArr.toString();
+				reportTask.processReport(strJobText);
+				//bsClient.deleteJob(job);
+			}
+		} catch (BeanstalkException e) {
+			e.printStackTrace();
+		} finally {
+			bsClient.close();
 		}
-		
-	
 	}
 
 }
