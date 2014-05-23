a105b23453c81defd95b3f5779457d8b2a11b2c6
zino <smile.zino@gmail.com>
2014-05-23 15:52:45 +0800

    maven to gradle

--- LogAnalyzer/src/main/java/tv/joyplus/backend/huan/LogTransforLauncher.java
+++ LogAnalyzer/src/main/java/tv/joyplus/backend/huan/LogTransforLauncher.java
@@ -1,24 +1,27 @@
 package tv.joyplus.backend.huan;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 
 public class LogTransforLauncher {
+	private final static Log log = LogFactory.getLog(LogTransforLauncher.class);
 	private JobLauncher jobLauncher;
 	private Job job;
 
 	public void execute() {
 		try {
-			System.out.println("start");
+			log.debug("job start");
 			JobParameters jobParameters = 
 					  new JobParametersBuilder()
 					  .addLong("time",System.currentTimeMillis()).toJobParameters();
 			jobLauncher.run(job, jobParameters);
-			System.out.println("done");
+			log.debug("job done");
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error(e.getMessage());
 		}
 	}
 	public void setJobLauncher(JobLauncher jobLauncher) {
